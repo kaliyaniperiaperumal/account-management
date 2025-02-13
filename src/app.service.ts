6df0c55c06b50659b31676d29043922e295d347b
Kaliyani Periaperumal <kaliyani.periaperumal@nagarro.com>
2025-02-13 11:39:55 +0530

    Cusotm Exception Filter added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    //return 'Hello World!';
+    throw new HttpException(
+      'This is a custom error',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
   }
 }
